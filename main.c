5a543207d6ae935833dc90c63f62f85144694839
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-01-27 01:45:04 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int a;
+
+    a=1;
+
+    while(a<=100)
+    {
+        printf("%d\n",a);
+        a=a+2;
+    }
+
+    getch();
+    return 0;
+}
